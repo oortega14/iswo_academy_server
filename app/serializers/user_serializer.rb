21feb59eb468540b6b3e2ee4434d46164573fdec
Oscar Ortega <ortegaoscar14@gmail.com>
2025-02-15 12:36:35 -0500

    :construction: add logic to the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,8 +10,23 @@ class UserSerializer < BaseSerializer
     {
       id: resource.id,
       email: resource.email,
-      created_at: resource.created_at,
-      updated_at: resource.updated_at
+      user_detail: {
+        first_name: resource.user_detail&.first_name,
+        last_name: resource.user_detail&.last_name,
+        birth_date: resource.user_detail&.birth_date,
+        phone: resource.user_detail&.phone,
+        dni: resource.user_detail&.dni,
+        gender: resource.user_detail&.gender,
+        username: resource.user_detail&.username,
+        address: {
+          address: resource.user_detail&.address&.address,
+          city: resource.user_detail&.address&.city,
+          province: resource.user_detail&.address&.province,
+          country: resource.user_detail&.address&.country,
+          postal_code: resource.user_detail&.address&.postal_code
+        },
+        social_networks: resource.user_detail&.social_networks
+      }
     }
   end
 end
